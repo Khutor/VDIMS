e5513e96e42f8baaa397bce0c3af78e8a85a77fd
Elliot Golden <elliotgolden@Elliots-MacBook-Air.local>
2018-04-24 12:15:58 -0500

    Add admin pages
    Add base admin inventory page

---
+++ VDIMS/admin/inventory.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/master_page.master" CodeFile="inventory.aspx.cs" Inherits="VDIMS.admin.inventory" %>
+<asp:Content runat="server" ContentPlaceHolderID="contentMain">
+    <h1>Current Inventory</h1>
+    <% 
+    
+    do_table();
+
+    %>
+</asp:Content>
---
+++ VDIMS/admin/inventory.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+namespace VDIMS.admin
+{
+
+    public partial class inventory : System.Web.UI.Page
+    {
+        public void do_table()
+        {
+            Response.Write("<p>This is going to a select all statement for the inventory similar to the general search results</p>");
+        }
+    }
+}
